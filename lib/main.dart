14456bcddd36a086270b4205eb166279565f5b88
kobewang <517686051@qq.com>
2018-12-25 18:27:51 +0800

    'd'

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './pages/NewsListPage.dart';
+import './pages/DiscoveryPage.dart';
+import './pages/TweetsListPage.dart';
+import './pages/MyInfoPage.dart';
+import './widgets/MyDrawer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {  
+  @override
+  MyAppState createState()=> MyAppState();
+}
+class MyAppState extends State<MyApp> {
+  int _tabIndex = 0;//页面当前选中的Tab的索引
+  var tabImages;
+  var appBarTitles = ['资讯','动弹','发现','我的'];
+  final tabTextStyleNormal = new TextStyle(color:  const Color(0xff969696));//未选中字体颜色
+  final tabTextStyleSelected = new TextStyle(color: const Color(0xff63ca6c));//选中字体
+  var _body;
+  Image getTabImage(path) {
+    return new Image.asset(path,width:20.0,height:20.0);
+  }
+  //数据初始化
+    void initData() {
+      if(tabImages == null) {
+        tabImages = [
+          [getTabImage('images/ic_nav_news_normal.png'),getTabImage('images/ic_nav_news_actived.png')],
+          [getTabImage('images/ic_nav_tweet_normal.png'),getTabImage('images/ic_nav_tweet_actived.png')],
+          [getTabImage('images/ic_nav_discover_normal.png'),getTabImage('images/ic_nav_discover_actived.png')],
+          [getTabImage('images/ic_nav_my_normal.png'),getTabImage('images/ic_nav_my_pressed.png')],          
+        ];
+      }
+     _body = new IndexedStack(    
+      children: <Widget>[
+        new NewsListPage(),
+        new TweetsListPage(),
+        new DiscoveryPage(),
+        new MyInfoPage()
+      ],
+      index: _tabIndex,
+      );
+    }
+    
+    Image getTabIcon(int curIndex) {
+      if(curIndex == _tabIndex) return tabImages[curIndex][1];
+      return tabImages[curIndex][0];
+    }
+    TextStyle getTabTextStyle(int curIndex) {
+      if(curIndex == _tabIndex) return tabTextStyleSelected;
+      return tabTextStyleNormal; 
+    }
+    Text getTabTitle(int curIndex) {
+      return new Text(appBarTitles[curIndex],style: getTabTextStyle(curIndex));
+    }
+
+    List<BottomNavigationBarItem> getBottomNavItems() {
+      List<BottomNavigationBarItem> list= new List();
+      for(int i=0; i<4; i++) {
+        list.add(new BottomNavigationBarItem(icon: getTabIcon(i),title: getTabTitle(i)));
+      }
+      return list;
+    }
+  @override
+  Widget build(BuildContext context) {    
+    initData();     
+    return new MaterialApp(
+      theme: new ThemeData(primaryColor: const Color(0xFF63CA6C)),//设置页面的主题色
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('My OSC',style: new TextStyle(color: Colors.white)), //设置APPBAR的标题
+          iconTheme: new IconThemeData(color: Colors.white)//设置APPBAR上图标的样式          
+         ),
+         body: _body, 
+         bottomNavigationBar: new  CupertinoTabBar(
+           items: getBottomNavItems(),
+           currentIndex: _tabIndex,
+           onTap: (index) {setState(() {_tabIndex=index;});},// 底部TabItem的点击事件处理，点击时改变当前选择的Tab的索引值，则页面会自动刷新
+         ),// bottomNavigationBar属性为页面底部添加导航的Tab，CupertinoTabBar是Flutter提供的一个iOS风格的底部导航栏组件
+         drawer: new MyDrawer(),
+      ),
+    );
+  }
+}
+
---
+++ lib/widgets/MyDrawer.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../pages/DiscoveryPage.dart';
+class MyDrawer extends StatelessWidget {
+  static const double IMAGE_ICON_WIDTH = 30.0;
+  static const double ARROW_ICON_HEIGHT = 16.0;
+  var rightArrowIcon = new Image.asset('images/ic_arrow_right.png',width:ARROW_ICON_HEIGHT,height:ARROW_ICON_HEIGHT);
+  List menuTitles = ['发布动弹','动弹小黑屋','关于','设置'];    //菜单文本
+  List menuIcons=[
+    './images/leftmenu/ic_fabu.png',
+    './images/leftmenu/ic_xiaoheiwu.png',
+    './images/leftmenu/ic_about.png',
+    './images/leftmenu/ic_settings.png'
+  ];//菜单图标
+  TextStyle menuStyle = new TextStyle( fontSize: 15.0);
+  @override
+  Widget build(BuildContext context) {
+    return new ConstrainedBox(
+      constraints: new BoxConstraints.expand(width: 304.0),//指定了侧滑菜单的宽度
+      child: new Material(
+        elevation: 16.0,//Drawer后面的阴影的大小，默认值就是16
+        child: new Container(
+          decoration: new BoxDecoration(color:new Color(0xFFFFFFFF)),
+          child: new ListView.builder(
+            itemCount: menuTitles.length*2+1,//大图+间隔
+            itemBuilder: renderRow,
+          )
+        )        
+      )
+    );//实现Drawer类的源码
+  }
+  Widget getIconImage(String path) {
+    return new Padding(
+      padding: new EdgeInsets.fromLTRB(2.0, 0.0, 6.0, 0.0),
+      child: new Image.asset(path,width:28.0,height:28.0),
+    );
+  }
+  Widget renderRow(BuildContext context, int index) {
+    if (index==0) {
+      return new Container(
+        width: 304.0,
+        height: 304.0,
+        margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+        child: new Image.asset('images/cover_img.jpg',width:304.0,height:304.0),
+      );
+    }
+      if (index.isOdd) {
+        return new Divider();
+      } //奇数
+      index-=1;
+      index = index ~/2;//除2取整
+      //菜单item组件
+      var listItemContent = new Padding(
+        padding: new EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: new Row(
+          children: <Widget>[
+            getIconImage(menuIcons[index]),//图标
+            new Expanded(child: new Text(menuTitles[index],style: menuStyle)),
+            rightArrowIcon
+          ],
+        ),
+      );
+      return new InkWell(
+        child: listItemContent,
+        onTap: (){
+          switch(index) {
+            case 0:Navigator.of(context).push(new MaterialPageRoute(builder: (ctx){ return new DiscoveryPage(); })); break;
+            case 1:Navigator.of(context).push(new MaterialPageRoute(builder: (ctx){ return new DiscoveryPage(); })); break;
+            case 2:Navigator.of(context).push(new MaterialPageRoute(builder: (ctx){ return new DiscoveryPage(); })); break;
+            case 3:Navigator.of(context).push(new MaterialPageRoute(builder: (ctx){ return new DiscoveryPage(); })); break;
+          }
+        },
+      );
+    
+  }
+}
